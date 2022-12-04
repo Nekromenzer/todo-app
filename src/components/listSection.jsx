41e31e1d058eec7eb0b8c31e28c058cf1518c29f
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-12-04 13:05:27 +0530

    list

---
+++ src/components/listSection.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ListSection = () => {
+  return (
+    <>
+      <div className='flex mb-4 items-center bg-green-50 px-3 py-1 border border-amber rounded cursor-pointer'>
+        <p className='w-full text-grey-darkest'>
+          Add another component to Tailwind Components
+        </p>
+        <button className='flex-no-shrink px-2 py-1 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green'>
+          ✔️
+        </button>
+        <button className='flex-no-shrink px-2 py-1 border-2 rounded text-red border-red hover:text-white hover:bg-red'>
+          Delete
+        </button>
+      </div>
+      <div className='flex mb-4 items-center bg-red-50 px-3 py-1 border border-amber rounded cursor-pointer'>
+        <p className='w-full text-grey-darkest line-through'>
+          Add another component to Tailwind Components
+        </p>
+        <button className='flex-no-shrink px-2 py-1 ml-4 mr-2 border-2 rounded hover:text-white text-green border-orange hover:bg-orange-300'>
+          ❌
+        </button>
+        <button className='flex-no-shrink px-2 py-1 border-2 rounded text-red border-red hover:text-white hover:bg-red'>
+          Delete
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default ListSection
