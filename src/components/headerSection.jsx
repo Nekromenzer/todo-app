6f72814be6a0d68db51965b4beb02a62dc58a618
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-12-04 13:54:08 +0530

    filter value

--- src/components/headerSection.jsx
+++ src/components/headerSection.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
-const HeaderSection = () => {
+const HeaderSection = ({ todoList, setTodoList, filterVal, setFilterVal }) => {
+  const handleSelect = e => {
+    setFilterVal(e.target.value)
+  }
+
+  const selectValue = [
+    { value: 'all', name: 'All' },
+    { value: 'completed', name: 'Completed' },
+    { value: 'uncompleted', name: 'Uncompleted' }
+  ]
+
   return (
     <div className='mb-8'>
       <h1 className='text-grey-darkest text-3xl'>Todo List</h1>
@@ -12,10 +22,21 @@ const HeaderSection = () => {
         <button className='p-2 border-2 rounded text-teal-900 border-teal hover:text-white hover:bg-teal-700 bg-teal'>
           Add
         </button>
-        <select className='shadow appearance-none border rounded py-2 px-3  text-grey-darker flex-no-grow'>
-          <option>All</option>
-          <option>Completed</option>
-          <option>Not completed</option>
+        <select
+          className='shadow appearance-none border rounded py-2 px-3  text-grey-darker flex-no-grow'
+          onChange={e => handleSelect(e)}
+          value={filterVal}
+        >
+          {selectValue.map((item, index) => (
+            <option value={item.value} key={`filter_option${index}`}>
+              {item.name}
+            </option>
+          ))}
+          {/* <option value='all'>
+            All
+          </option>
+          <option value='completed'>Completed</option>
+          <option value='uncompleted'>Uncompleted</option> */}
         </select>
       </div>
     </div>
