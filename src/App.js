1e6eba6bffbb8aefce72f33b7729ef82743ce4d4
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-12-04 18:47:51 +0530

    store local storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import HeaderSection from './components/headerSection'
+import UseDidMountEffect from './components/hooks/useDidMountEffect'
 import ListSection from './components/listSection'
 
 function App () {
   const [todoList, setTodoList] = useState([])
   const [filterVal, setFilterVal] = useState('all')
+
+  // useEffect(() => {
+  //   localStorage.setItem('todoList', JSON.stringify(todoList))
+  // }, [todoList])
+
+  UseDidMountEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(todoList))
+  }, [todoList])
+  
+  useEffect(() => {
+    setTodoList(JSON.parse(localStorage.getItem('todoList')))
+  }, [])
+
   return (
     <div className='App'>
       <div className='h-screen overflow-hidden flex items-center justify-center bg-cyan-50'>
@@ -16,7 +30,11 @@ function App () {
               filterVal={filterVal}
               setFilterVal={setFilterVal}
             />
-            <ListSection todoList={todoList} setTodoList={setTodoList} filterVal={filterVal} />
+            <ListSection
+              todoList={todoList}
+              setTodoList={setTodoList}
+              filterVal={filterVal}
+            />
           </div>
         </div>
       </div>
