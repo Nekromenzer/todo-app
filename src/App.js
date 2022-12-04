4b0f3353ff2f7f248265d0b2dd35f854e56466a0
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-12-04 13:54:15 +0530

    pass props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React, { useState } from 'react'
 import './App.css'
 import HeaderSection from './components/headerSection'
 import ListSection from './components/listSection'
 
 function App () {
+  const [todoList, setTodoList] = useState([])
+  const [filterVal, setFilterVal] = useState('all')
   return (
     <div className='App'>
       <div className='h-screen overflow-hidden flex items-center justify-center bg-cyan-50'>
         <div className='h-100 w-full flex items-center justify-center font-sans'>
           <div className='bg-white rounded shadow-lg p-6 m-4 w-full lg:w-1/2 '>
-            <HeaderSection />
+            <HeaderSection
+              todoList={todoList}
+              setTodoList={setTodoList}
+              filterVal={filterVal}
+              setFilterVal={setFilterVal}
+            />
             <ListSection />
           </div>
         </div>
