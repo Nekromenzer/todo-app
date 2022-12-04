3c7acd998b8d234155c3b5fef6ba8bf4d5d831ea
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-12-04 14:24:00 +0530

    add task to array

--- src/components/headerSection.jsx
+++ src/components/headerSection.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 const HeaderSection = ({ todoList, setTodoList, filterVal, setFilterVal }) => {
-  const handleSelect = e => {
-    setFilterVal(e.target.value)
-  }
+  const [task, setTask] = useState('')
 
   const selectValue = [
     { value: 'all', name: 'All' },
@@ -11,20 +10,31 @@ const HeaderSection = ({ todoList, setTodoList, filterVal, setFilterVal }) => {
     { value: 'uncompleted', name: 'Uncompleted' }
   ]
 
+  const handleAddTask = () => {
+    setTodoList(pre => [...pre, { task, id: uuidv4() }])
+    // reset the input value
+    setTask('')
+  }
+
   return (
     <div className='mb-8'>
       <h1 className='text-grey-darkest text-3xl'>Todo List</h1>
       <div className='flex mt-4 gap-5'>
         <input
           className='shadow appearance-none border rounded w-full py-2 px-3  text-grey-darker'
-          placeholder='Add Todo'
+          placeholder='Add task'
+          value={task}
+          onChange={e => setTask(e.target.value)}
         />
-        <button className='p-2 border-2 rounded text-teal-900 border-teal hover:text-white hover:bg-teal-700 bg-teal'>
+        <button
+          className='p-2 border-2 rounded text-teal-900 border-teal hover:text-white hover:bg-teal-700 bg-teal'
+          onClick={() => handleAddTask()}
+        >
           Add
         </button>
         <select
           className='shadow appearance-none border rounded py-2 px-3  text-grey-darker flex-no-grow'
-          onChange={e => handleSelect(e)}
+          onChange={e => setFilterVal(e)}
           value={filterVal}
         >
           {selectValue.map((item, index) => (
